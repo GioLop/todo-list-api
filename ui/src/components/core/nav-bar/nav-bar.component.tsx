355275713da7eba352931ebbb7c27c9c5a66451b
Giovanni <giovanni@192.168.0.103>
2024-10-23 16:58:28 -0500

    UI: Add task button in nav

--- ui/src/components/core/nav-bar/nav-bar.component.tsx
+++ ui/src/components/core/nav-bar/nav-bar.component.tsx
@@ -1,16 +1,17 @@
-import { FC, MouseEvent, useLayoutEffect, useRef, useState } from "react";
+import { FC, MouseEvent, ReactNode, useLayoutEffect, useRef, useState } from "react";
 import NavButton from "../nav-button/nav-button.component";
 import NavIndicator from "../nav-indicator/nav-indicator.component";
 import './nav-bar.component.scss'
-
-type NavBarType = {
-    onItemChange: () => void
-};
 
 type NavItemType = {
     name: string,
     selected: boolean,
     value: string
+};
+
+type NavBarType = {
+    additionalElement?: ReactNode,
+    onItemChange: (navItem:NavItemType) => void
 };
 
 const listItems = [
@@ -36,7 +37,7 @@ const listItems = [
     },
 ];
 
-const NavBar:FC<NavBarType> = () => {
+const NavBar:FC<NavBarType> = ({ additionalElement, onItemChange }) => {
     const [ items, setItems ] = useState(listItems);
     const [ carWidth, setCarWidth] = useState(0);
     const [ carLeft, setcarLeft] = useState(0);
@@ -47,10 +48,10 @@ const NavBar:FC<NavBarType> = () => {
         setItems([...items].map((_item) => ({ ..._item, selected: item.name === _item.name ? true : false })))
         setCarWidth((event.target as HTMLButtonElement).clientWidth);
         setcarLeft((event.target as HTMLButtonElement).offsetLeft);
+        onItemChange(item);
     };
 
     useLayoutEffect(() => {
-        console.log(buttonRefs);
         const selectedIndex = items.findIndex(item => item.selected);
         const selectedButton = buttonRefs.current[selectedIndex];
 
@@ -62,16 +63,20 @@ const NavBar:FC<NavBarType> = () => {
 
     return (
         <nav className="nav-bar">
-            <ul className="nav-bar__list">
-                {items.map((item, index) => (
-                    <li className="nav-bar__item" key={`nav-item-${item.name}`}>
-                        <NavButton
-                            selected={item.selected}
-                            onClick={handleItemClick(item)}
-                            ref={(el) => buttonRefs.current[index] = el!}>{item.name}</NavButton>
-                    </li>
-                ))}
-            </ul>
+            <div className="nav-bar__first-line">
+                <ul className="nav-bar__list">
+                    {items.map((item, index) => (
+                        <li className="nav-bar__item" key={`nav-item-${item.name}`}>
+                            <NavButton
+                                selected={item.selected}
+                                onClick={handleItemClick(item)}
+                                ref={(el) => buttonRefs.current[index] = el!}>{item.name}</NavButton>
+                        </li>
+                    ))}
+                </ul>
+                { additionalElement }
+            </div>
+            
             <NavIndicator left={carLeft} width={carWidth} />
         </nav> 
     );
--- ui/src/pages/todo-list/todo-list.page.tsx
+++ ui/src/pages/todo-list/todo-list.page.tsx
@@ -2,14 +2,22 @@ import { FC } from "react";
 import Link from "../../components/core/link/link.component";
 import NavBar from "../../components/core/nav-bar/nav-bar.component";
 import Header from "../../components/core/header/header.component";
+import Button from "../../components/core/button/button.component";
 
 const TodoList:FC = () => {
+
+    const handleAddTaskClick = () => {
+        console.log('Add Task click');
+    };
+    
+    const addtaskButton = <Button onClick={handleAddTaskClick} value="Add Task"/>;
+    
     return (
         <>
             <Header 
                 message={'giovanni.helion@gmail.com'}
-                element={<Link>Logout</Link>}/>
-            <NavBar onItemChange={() => {}}/>
+                element={<Link >Logout</Link>}/>
+            <NavBar onItemChange={() => {}} additionalElement={addtaskButton}/>
             <main className='main'></main>
         </>
     );
